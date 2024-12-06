d8a24756c8f23efa6b2296bdcd098f2b7590c12f
HerculesOl <147669122+HerculesOl@users.noreply.github.com>
2024-12-05 21:52:11 -0300

    first

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-HAHAHAHHAHAHHAHAHAH
+/**
+ * Express - npm install express
+ * Sequelize - npm install sequelize
+ * nodemom - npm install nodemom
+ * Body-parser - npm install body-parser
+ * handlebars - npm install handlebars
+ */
+
+
+const express = require('express')
+const { dirname } = require('path')
+const app = express()
+const path = require('path')
+const port = 8080
+
+
+
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + "/index.html")
+})
+
+
+
+app.listen(8080,function(){
+    console.log("Servidor conectado  http://localhost:"+port)
+})
