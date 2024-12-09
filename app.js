710ba258d870f5e11888c17b0fcd12367ad9cba2
Marina2447 <147669230+Marina2447@users.noreply.github.com>
2024-12-08 22:45:19 -0300

    Atualizado

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const handlebars = require("express-handlebars");
-const path = require('path');
 
 // Configurar o Handlebars
 app.engine('handlebars', handlebars.engine({
-  defaultLayout: 'main',
-  runtimeOptions: {
-      allowProtoPropertiesByDefault: true,
-      allowProtoMethodsByDefault: true
-  },
-  partialsDir: path.join(__dirname, 'views/partials')  
+    defaultLayout: 'main',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true
+    }
 }));
 app.set('view engine', 'handlebars');
 
@@ -27,15 +25,13 @@ app.listen(port, () => {
     console.log("Servidor rodando na URL http://localhost:" + port);
 });
 
+
+
 const Post = require('./models/Post');
 
 // Formulário de cadastro
 app.get('/cadastro', (req, res) => {
     res.render('formulario');
-});
-
-app.get('/contato', (req, res) => {
-  res.render('contato'); // A página de contato será renderizada
 });
 
 // CREATE - Adicionar novo post
@@ -83,3 +79,169 @@ app.post('/edit', (req, res) => {
         res.redirect('/');
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const app = express();
+// const bodyParser = require("body-parser");
+// const handlebars = require("express-handlebars");
+// const path = require('path');
+
+// // Configurar o Handlebars
+// app.engine('handlebars', handlebars.engine({
+//   defaultLayout: 'main',
+//   runtimeOptions: {
+//       allowProtoPropertiesByDefault: true,
+//       allowProtoMethodsByDefault: true
+//   },
+//   partialsDir: path.join(__dirname, 'views/partials')  
+// }));
+// app.set('view engine', 'handlebars');
+
+// // Configurar Body Parser
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// // Configurar arquivos estáticos
+// app.use(express.static("assets"));
+
+// const port = 8083;
+// app.listen(port, () => {
+//     console.log("Servidor rodando na URL http://localhost:" + port);
+// });
+
+// const Post = require('./models/Post');
+
+// // Formulário de cadastro
+// app.get('/cadastro', (req, res) => {
+//     res.render('formulario');
+// });
+
+// app.get('/contato', (req, res) => {
+//   res.render('contato'); // A página de contato será renderizada
+// });
+
+// // CREATE - Adicionar novo post
+// app.post('/add', (req, res) => {
+//     Post.create({
+//         titulo: req.body.titulo,
+//         conteudo: req.body.conteudo
+//     }).then(() => {
+//         res.render('add');
+//     }).catch(err => {
+//         res.send("Erro ao criar post: " + err);
+//     });
+// });
+
+// // READ - Exibir posts
+// app.get('/', (req, res) => {
+//     Post.findAll().then(posts => {
+//         res.render('home', { posts: posts });
+//     });
+// });
+
+// // DELETE - Remover post
+// app.get('/deletar/:id', (req, res) => {
+//     Post.destroy({ where: { id: req.params.id } }).then(() => {
+//         res.render('delete', { msg: "Postagem deletada com sucesso!" });
+//     }).catch(err => {
+//         res.render('delete', { msg: "Erro ao deletar postagem: " + err });
+//     });
+// });
+
+// // EDITAR - Pegar dados para edição
+// app.get('/edit/:id', (req, res) => {
+//     Post.findOne({ where: { id: req.params.id } }).then(post => {
+//         res.render('editposts', { post: post });
+//     });
+// });
+
+// // UPDATE - Salvar edições
+// app.post('/edit', (req, res) => {
+//     Post.findOne({ where: { id: req.body.id } }).then(post => {
+//         post.titulo = req.body.titulo;
+//         post.conteudo = req.body.conteudo;
+//         return post.save();
+//     }).then(() => {
+//         res.redirect('/');
+//     });
+// });
