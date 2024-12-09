7fcf499edb39f3792b03637ef27710a193b6c582
Marina2447 <marinaalmeida887@gmail.com>
2024-12-09 20:52:53 -0300

    configurando

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize'); // Importação do Sequelize
 
 // Configuração da conexão com o banco de dados
-const sequelize = new Sequelize('sequelize','root','admin', {
+const sequelize = new Sequelize('blog','admin ','admin', {
     host: "localhost",
     dialect:'mysql'
 })
@@ -10,5 +10,7 @@ var db = {}; // Para armazenar a instância do Sequelize e a classe Sequelize
 
 db.sequelize = sequelize; // Armazena a instância conectada do Sequelize, que pode ser usada para executar operações no banco de dados.
 db.Sequelize = Sequelize; // Armazena a classe Sequelize, que pode ser útil para criar modelos ou para definir operações como DataTypes
+
+
 
 module.exports = db;
