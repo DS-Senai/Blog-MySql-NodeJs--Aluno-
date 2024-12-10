33dca61d42e35aec81f3df7e9938a052982aa261
Miquéias Sousa <miqueias.sousa.ti@gmail.com>
2024-12-10 12:10:51 -0300

    Correção

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize'); // Importação do Sequelize
 
-// const senha = 'Cleissiane2504';
-const senha = 'admin';
+const senha = 'Cleissiane2504';
+// const senha = 'admin';
 
 // Configuração da conexão com o banco de dados
 const sequelize = new Sequelize('sequelize','root',senha, {
